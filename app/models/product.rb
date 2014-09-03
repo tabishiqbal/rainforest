d5bbcda5f97f16c1653f804c13b4b31fb0fe5a3f
Tabish <tabishiqbal@outlook.com>
2014-09-02 20:41:04 -0400

    added associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ActiveRecord::Base
 	validates :description, :name, presence: true
 	validates :price_in_cents, numericality: {only_integer: true}
 
+	has_many :reviews
+	has_many :reviews, through: :reviews
 
 	def formatted_price
 		price_in_dollars = price_in_cents.to_f / 100
